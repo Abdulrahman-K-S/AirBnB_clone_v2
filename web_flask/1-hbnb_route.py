6fece7fabe25b2f992f7732ca2c0cfac9d4aacd0
Abdulrahman Khaled <AK-Salah@outlook.com>
2024-02-17 18:17:57 +0000

    Updated '1-hbnb_route.py'
    Placed the code under the if statment

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,20 +4,20 @@
 from flask import Flask
 
 
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """Display 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Display 'HBNB'
-    """
-    return 'HBNB'
-
 if __name__ == '__main__':
+    app = Flask(__name__)
+
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
+
     app.run('0.0.0.0')
